6b53d87f9fc60b59132c28b1a79aadeb5d58f000
aligomaa56 <3looosh56565656@gmail.com>
2024-06-04 02:50:02 +0300

    Optimize the code and make it readable, and add some helpful comments

--- website/models.py
+++ website/models.py
@@ -3,6 +3,16 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Post(db.Model):
+    """
+    This class represents a Post in the database.
+
+    Attributes:
+        id (int): The primary key.
+        data (str): The content of the post.
+        date (datetime): The date and time the post was created.
+        user_id (int): The ID of the user who created the post.
+        likes (relationship): The likes associated with the post.
+    """
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Text)
     date = db.Column(db.DateTime, default=func.now())
@@ -10,6 +20,22 @@ class Post(db.Model):
     likes = db.relationship("Like", backref="post", cascade="all, delete-orphan")
 
 class User(db.Model, UserMixin):
+    """
+    This class represents a User in the database.
+
+    Attributes:
+        id (int): The primary key.
+        email (str): The user's email address.
+        password (str): The user's password.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
+        profile_picture (str): The URL of the user's profile picture.
+        posts (relationship): The posts created by the user.
+        bio (str): The user's biography.
+        links (str): The user's social media links.
+        date_joined (datetime): The date and time the user joined.
+        likes (relationship): The likes given by the user.
+    """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
@@ -23,6 +49,14 @@ class User(db.Model, UserMixin):
     likes = db.relationship("Like", backref="user", cascade="all, delete-orphan")
 
 class Like(db.Model):
+    """
+    This class represents a Like in the database.
+
+    Attributes:
+        id (int): The primary key.
+        user_id (int): The ID of the user who gave the like.
+        post_id (int): The ID of the post that was liked.
+    """
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
